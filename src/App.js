d2959ae1e832712f316d769aa3e196e985920e12
binyamin.regev <beniregev@gmail.com>
2019-10-03 11:02:10 +0300

    Fixed bug #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/HomePage';
 import { RecipesPage } from './pages/HomePage';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-      <Route path="/login">
-        <LoginPage />
-      </Route>
-      <Route path="/recipes">
-        <RecipesPage />
-      </Route>
-    </Switch>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUser: null,
+    };
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/recipes">
+          <RecipesPage />
+        </Route>
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,20 @@
+
+
 class HomePage extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#/">Recipe Book</Navbar.Brand>
+                <Navbar.Toggle></Navbar.Toggle>
+            </Navbar>
+        );
+    }
 }
 
 export default HomePage;
