434790ac6b28a34c6f051fcbd4810407bc57f7ba
binyamin.regev <beniregev@gmail.com>
2019-10-06 09:52:04 +0300

    Commit changes up-to-date with class exercise

--- src/App.js
+++ src/App.js
@@ -1,36 +1,80 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { LoginPage } from './pages/LoginPage';
-import { RecipesPage } from './pages/RecipesPage';
+import { Switch, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RecipesPage from './pages/RecipesPage';
+import jsonUsers from './data/users'
+import jsonRecipes from './data/recipes'
+
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       activeUser: null,
-    };
+    //   activeUser:   {
+    //     "id": 1,
+    //     "fname": "Nir",
+    //     "lname": "Channes",
+    //     "email": "nir@nir.com",
+    //     "pwd": "123"
+    // },
+      allUsers: jsonUsers,
+      allRecipes: jsonRecipes,
+      activeUserRecipes: []
+      // hack for starting with my recipes
+      // activeUserRecipes: jsonRecipes.filter(recipe => recipe.userId === 1)
+    }
+
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.addRecipe = this.addRecipe.bind(this);
+
+    console.log(this.state.allRecipes);
   }
 
   handleLogout() {
-    this.setState({ activeUser: null });
+    this.setState({activeUser: null});
+  }
+
+  handleLogin(activeUser) {
+
+    const activeUserRecipes = this.state.allRecipes.filter(recipe => recipe.userId === activeUser.id)
+
+    this.setState({activeUser, activeUserRecipes});
+  }
+
+  addRecipe(newRecipe) {
+    //const {activeUser, allRecipes, activeUserRecipes} this.state.activeUser
+    // 1) add id and user to the recipe
+    newRecipe.userId = this.state.activeUser.id;
+    newRecipe.id = this.state.allRecipes[this.state.allRecipes.length - 1].id + 1;
+
+    // 2) update all recipes and active user recipes
+    const allRecipes = this.state.allRecipes.concat(newRecipe);
+    const activeUserRecipes = this.state.activeUserRecipes.concat(newRecipe);
+
+    this.setState({allRecipes, activeUserRecipes});
   }
 
   render() {
-    const { activeUser } = this.state;
+
+    const { activeUser, allUsers, activeUserRecipes } = this.state;
+    // const activeUser = this.state.activeUser;
 
     return (
       <Switch>
         <Route exact path="/">
-          <HomePage activeUser={activeUser} handleLogout={this.handleLogout} />
+          <HomePage activeUser={activeUser} handleLogout={this.handleLogout}/>
         </Route>
         <Route path="/login">
-          <LoginPage />
+          <LoginPage users={allUsers} handleLogin={this.handleLogin}/>
         </Route>
         <Route path="/recipes">
-          <RecipesPage activeUser={activeUser} handleLogout={this.handleLogout} />
+          <RecipesPage recipes={activeUserRecipes} activeUser={activeUser} handleLogout={this.handleLogout} addRecipe={this.addRecipe}/>
         </Route>
       </Switch>
     );
--- src/pages/RecipesPage.js
+++ src/pages/RecipesPage.js
@@ -1,21 +1,120 @@
-import React from 'react';
+import React from 'react'
+import RecipeNavbar from '../components/RecipeNavbar'
+import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap'
+import { Redirect } from 'react-router-dom'
+import RecipeCard from '../components/RecipeCard'
 
-export class RecipesPage extends React.Component {
 
+class RecipesPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            showModal: false
+        }
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.createRecipe = this.createRecipe.bind(this);
+
+
+        this.nameInput = React.createRef();
+        this.descInput = React.createRef();
+        this.imgInput = React.createRef();
+    }
+
+
+    openModal() {
+        this.setState({ showModal: true })
+    }
+
+    closeModal() {
+        this.setState({ showModal: false })
+    }
+
+    createRecipe() {
+        const newRecipe = {
+            name: this.nameInput.current.value,
+            desc: this.descInput.current.value,
+            img: this.imgInput.current.value,
+        }
+
+        this.props.addRecipe(newRecipe);
+        this.closeModal();
     }
 
     render() {
-        const { activeUser } = this.props;
+        const { activeUser, handleLogout, recipes } = this.props;
+        const { showModal } = this.state;
+        //const showModal = this.state.showModal;
 
-        return(
+        if (!activeUser) {
+            return <Redirect to="/" />
+        }
+
+        const recipesCards = recipes.map(recipe => <Col key={recipe.id} lg="3" md="6"><RecipeCard recipe={recipe} /></Col>)
+
+        return (
             <div>
-                Recipes Page Component
+                <RecipeNavbar activeUser={activeUser} handleLogout={handleLogout} />
+                <Container>
+                    <div className="recipes-header">
+                        <h1>{activeUser.fname}'s Recipes</h1>
+                        <Button variant="primary" onClick={this.openModal}>New Recipe</Button>
+                    </div>
+                    <Row>
+                        {recipesCards}
+                    </Row>
+                </Container>
+
+
+                <Modal show={showModal} onHide={this.closeModal} size="lg">
+                    <Modal.Header closeButton>
+                        <Modal.Title>New Recipe</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group as={Row} controlId="formHorizontalEmail">
+                                <Form.Label column sm={2}>
+                                    Name
+                                </Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control ref={this.nameInput} type="text" placeholder="Recipe name" />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId="formHorizontalPassword">
+                                <Form.Label column sm={2}>
+                                    Description
+                                </Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control ref={this.descInput} type="text" placeholder="Recipe description" />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId="formHorizontalPassword">
+                                <Form.Label column sm={2}>
+                                    Image URL
+                                </Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control ref={this.imgInput} type="text" placeholder="Recipe image URL" />
+                                </Col>
+                            </Form.Group>
+
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.closeModal}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.createRecipe}>
+                            Create Recipe
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
             </div>
         );
     }
 }
 
-// export default RecipesPage;
+export default RecipesPage;
